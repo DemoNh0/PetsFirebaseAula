f7d38a2595211b86db90ff355897cc9039f1a8c8
aluno <etecae023@hotmail.com>
2025-08-27 22:06:30 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Importações do SDK Modular do Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
+
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDSg5-6vOihOtOlg_4CmX7BX-PRiosgw4s",
+    authDomain: "ionicfirebase-39aa2.firebaseapp.com",
+    projectId: "ionicfirebase-39aa2",
+    storageBucket: "ionicfirebase-39aa2.firebasestorage.app",
+    messagingSenderId: "130580409777",
+    appId: "1:130580409777:web:9c33689a95072bbdace190",
+    measurementId: "G-RK2C0KNNHR"
+  }
 };
 
 /*
